b3d58370582718243eb5e08343cfe4eae7518f16
gamerus12125 <Mik78456@yandex.ru>
2023-12-25 17:38:58 +0300

    Changed collision, changed hitbox, animation, added attack animation

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import pygame
-
+import os
 import time
 
-objects = []
+all_sprites = pygame.sprite.Group()
 WIDTH = 1000
 HEIGHT = 1000
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
 class Interface:
@@ -18,7 +35,7 @@ class Interface:
         self.grenn = (0, 255, 0)
         self.player = player
 
-    def render(self, screen):
+    def render(self):
         ammo = self.font.render(str(self.player.ammo), True, self.red)
         health = self.font.render(str(self.player.hp), True, self.grenn)
         screen.blit(ammo, (self.x, self.y))
@@ -42,81 +59,50 @@ class Timer:
         return time.perf_counter() - self.start_time
 
 
-def check_intersect(obj1):
-    for itm in objects:
-        if type(obj1).__name__ == "Player":
-            if itm.is_player_collide:
-                if set(obj1.get_coords()[0]).intersection(itm.get_coords()[0]) and set(
-                        obj1.get_coords()[1]).intersection(
-                    itm.get_coords()[1]):
-                    return True
-        else:
-            if itm != obj1:
-                if set(obj1.get_coords()[0]).intersection(itm.get_coords()[0]) and set(
-                        obj1.get_coords()[1]).intersection(
-                    itm.get_coords()[1]):
-                    return True
-    return False
-
-
-class Wall:
+class Wall(pygame.sprite.Sprite):
     def __init__(self, w, h, x, y):
+        super().__init__(all_sprites)
         self.height = h
         self.width = w
         self.x = x
         self.y = y
         self.is_player_collide = True
+        self.rect = pygame.Rect(x, y, w, h)
 
-    def render(self, screen):
+    def update(self):
         pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, self.width, self.height))
 
-    def get_coords(self):
-        return range(self.x, self.x + self.width), range(self.y, self.y + self.height)
 
-
-class Bullet:
+class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, direction):
-        self.x = x
-        self.y = y
-        self.height = 10
-        self.width = 20
+        super().__init__(all_sprites)
+        self.rect = pygame.Rect(x, y, 20, 10)
         self.is_player_collide = False
-        self.direction = direction
+        self.vx = -4 if direction == "L" else 4
 
-    def render(self, screen):
-        if check_intersect(self):
-            objects.remove(self)
+    def update(self):
+        colliders = pygame.sprite.spritecollide(self, all_sprites, False)
+        if colliders and Wall.__name__ in colliders.__str__():
             del self
         else:
-            pygame.draw.rect(screen, pygame.color.Color("red"), (self.x, self.y, self.width, self.height))
-            if self.direction == "L":
-                self.x -= 4
-            else:
-                self.x += 4
-            if self.x > WIDTH:
+            pygame.draw.rect(screen, pygame.color.Color("red"), self.rect)
+            self.rect = self.rect.move(self.vx, 0)
+            if self.rect.x > WIDTH:
                 del self
 
-    def get_coords(self):
-        return range(self.x, self.x + self.width), range(self.y, self.y + self.height)
 
-
-class Player:
+class Player(pygame.sprite.Sprite):
     def __init__(self, w, h, x, y, animation):
-        self.height = h
-        self.width = w
-        self.x = x
-        self.y = y
+        super().__init__(all_sprites)
         self.hp = 100
+        self.rect = pygame.rect.Rect(x, y, w, h)
 
-        self.is_jump = False
-        self.max_jump = 200
-        self.jump_height = 0
         self.is_down = False
         self.is_walk_r = False
         self.is_walk_l = False
         self.is_idle = True
+        self.is_attack = False
         self.g = 0.2
-        self.jump_speed = 0
         self.fall_speed = 0
 
         self.ammo = 10
@@ -126,61 +112,48 @@ class Player:
         self.shoot_clock.start()
         self.animations = animation
 
-        self.images = {"idle_r": [], "idle_l": [], "run_r": [], "run_l": []}
-        for i in range(0, 97, 32):
-            self.images["idle_r"].append(
-                pygame.transform.scale(animation["idle"].subsurface(pygame.Rect(i, 0, 32, 32)),
-                                       (self.width, self.height)))
-            self.images["idle_l"].append(
-                pygame.transform.flip(pygame.transform.scale(animation["idle"].subsurface(pygame.Rect(i, 0, 32, 32)),
-                                                             (self.width, self.height)), True, False))
+        self.images = {"idle_r": [], "idle_l": [], "run_r": [], "run_l": [], "attack_r": [], "attack_l": []}
 
-        for i in range(0, 160, 32):
-            self.images["run_r"].append(
-                pygame.transform.scale(animation["run"].subsurface(pygame.Rect(i, 0, 32, 32)),
-                                       (self.width, self.height)))
-            self.images["run_l"].append(
-                pygame.transform.flip(pygame.transform.scale(animation["run"].subsurface(pygame.Rect(i, 0, 32, 32)),
-                                                             (self.width, self.height)), True, False))
+        self.init_images(animation)
+
         self.index = 0
         self.image = self.images["idle_r"][self.index]
-        self.animation_time = 0.1
+        self.animation_time = 0.15
+
         self.anim_cur_time = Timer()
         self.anim_cur_time.start()
 
-    def get_coords(self):
-        return range(self.x, self.x + self.width), range(int(self.y) - int(self.jump_height),
-                                                         int(self.y) + self.height - int(self.jump_height))
+    def init_images(self, animation):
+        for i in range(0, 128, 32):
+            self.images["idle_r"].append(
+                pygame.transform.scale_by(animation["idle"].subsurface(pygame.Rect(i, 0, 32, 32)), 3))
+            self.images["idle_l"].append(
+                pygame.transform.flip(
+                    pygame.transform.scale_by(animation["idle"].subsurface(pygame.Rect(i, 0, 32, 32)), 3), True, False))
 
-    def stop_jump(self):
-        self.y -= self.jump_height
-        self.jump_height = 0
-        self.is_down = True
-        self.is_jump = False
+        for i in range(0, 181, 32):
+            self.images["run_r"].append(
+                pygame.transform.scale_by(animation["run"].subsurface(pygame.Rect(i, 0, 32, 32)), 3))
+            self.images["run_l"].append(
+                pygame.transform.flip(pygame.transform.scale_by(animation["run"].subsurface(pygame.Rect(i, 0, 32, 32)),
+                                                                3), True, False))
+        for i in range(0, 97, 32):
+            self.images["attack_r"].append(
+                pygame.transform.scale_by(animation["attack"].subsurface(pygame.Rect(i, 0, 32, 32)), 3))
+            self.images["attack_l"].append(
+                pygame.transform.flip(
+                    pygame.transform.scale_by(animation["attack"].subsurface(pygame.Rect(i, 0, 32, 32)), 3), True,
+                    False))
 
-    def render(self, screen, keys):
-        if self.is_jump:
-            if self.jump_height >= self.max_jump:
-                self.stop_jump()
-            else:
-                self.jump_speed -= self.g
-                if self.jump_speed < 0:
-                    self.stop_jump()
-                else:
-                    self.jump_height += self.jump_speed
-        else:
-            self.fall_speed += self.g
-            self.y += self.fall_speed
-
-        if check_intersect(self):
-            if self.is_jump:
-                self.jump_height -= 6
-                self.is_jump = False
-                self.is_down = True
-            else:
-                self.y -= self.fall_speed
-                self.fall_speed = 0
-                self.is_down = False
+    def update(self):
+        self.fall_speed += self.g
+        self.rect = self.rect.move(0, self.fall_speed)
+        colliders = pygame.sprite.spritecollide(self, all_sprites, False)
+        colliders.remove(self)
+        if colliders and Wall.__name__ in colliders.__str__():
+            self.rect = self.rect.move(0, -self.fall_speed)
+            self.fall_speed = 0
+            self.is_down = False
         else:
             self.is_down = True
 
@@ -189,32 +162,41 @@ class Player:
                 self.is_reload = False
                 self.ammo = 10
 
-        self.move(keys, screen)
+        self.move()
 
-    def animation(self, type, screen):
+    def animation(self, name):
+        anim = name
         if self.anim_cur_time.get_time() > self.animation_time:
             self.anim_cur_time.stop()
             self.anim_cur_time.start()
             self.index += 1
-            if self.index >= len(self.images[type]):
+
+            if self.index >= len(self.images[anim]):
+                if self.is_attack:
+                    self.is_attack = False
                 self.index = 0
-            self.image = self.images[type][self.index]
-        screen.blit(self.images[type][self.index], (self.x, self.y - self.jump_height))
+            self.image = self.images[anim][self.index]
+        if self.is_attack:
+            anim = "attack_r"
+        screen.blit(self.images[anim][self.index], (self.rect.x - 16, self.rect.y - 32))
+        pygame.draw.rect(screen, (255, 0, 0), self.rect, 1)
 
     def jump(self):
         if not self.is_down:
-            self.jump_speed = 10
-            self.is_jump = True
+            self.fall_speed = -10
 
-    def move(self, keys, screen):
+    def move(self):
+        if keys[pygame.K_SPACE]:
+            self.jump()
         if keys[pygame.K_d]:
-            self.x += 2
+            self.rect = self.rect.move(2, 0)
             self.direction = "R"
-            if check_intersect(self):
-                self.x -= 2
+            is_collide = pygame.sprite.spritecollideany(self, all_sprites)
+            if is_collide and is_collide != self:
+                self.rect = self.rect.move(-2, 0)
                 if self.is_walk_l or self.is_walk_r:
                     self.index = 0
-                self.animation("idle_r", screen)
+                self.animation("idle_r")
                 self.is_walk_r = False
                 self.is_walk_l = False
                 return False
@@ -223,17 +205,18 @@ class Player:
                 self.is_walk_r = True
                 if self.is_idle:
                     self.index = 0
-                self.animation("run_r", screen)
+                self.animation("run_r")
                 self.is_idle = False
                 return True
         elif keys[pygame.K_a]:
-            self.x -= 2
+            self.rect = self.rect.move(-2, 0)
             self.direction = "L"
-            if check_intersect(self):
-                self.x += 2
+            is_collide = pygame.sprite.spritecollideany(self, all_sprites)
+            if is_collide and is_collide != self:
+                self.rect = self.rect.move(2, 0)
                 if self.is_walk_l or self.is_walk_r:
                     self.index = 0
-                self.animation("idle_l", screen)
+                self.animation("idle_l")
                 self.is_walk_r = False
                 self.is_walk_l = False
                 return False
@@ -242,31 +225,32 @@ class Player:
                 self.is_walk_r = False
                 if self.is_idle:
                     self.index = 0
-                self.animation("run_l", screen)
+                self.animation("run_l")
                 self.is_idle = False
                 return True
         else:
             if self.is_walk_l or self.is_walk_r:
                 self.index = 0
             if self.direction == "R":
-                self.animation("idle_r", screen)
+                self.animation("idle_r")
             else:
-                self.animation("idle_l", screen)
+                self.animation("idle_l")
             self.is_walk_r = False
             self.is_walk_l = False
 
     def shoot(self):
         if self.shoot_clock.get_time() > 0.5 and not self.is_reload:
+            self.is_attack = True
+            self.index = -1
             if not self.ammo:
                 self.is_reload = True
                 self.shoot_clock.stop()
                 self.shoot_clock.start()
             else:
                 if self.direction == "L":
-                    objects.append(Bullet(self.x - 1, int(self.y + 5) - int(self.jump_height), self.direction))
+                    Bullet(self.rect.x - 1, int(self.rect.y + 5), self.direction)
                 else:
-                    objects.append(
-                        Bullet(self.x + self.width + 1, int(self.y + 5) - int(self.jump_height), self.direction))
+                    Bullet(self.rect.x + self.rect.w + 1, int(self.rect.y + 5), self.direction)
                 self.ammo -= 1
                 self.shoot_clock.stop()
                 self.shoot_clock.start()
@@ -280,23 +264,13 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(size)
 
-    player_animations = {"idle": pygame.image.load("data/Pink_Monster_Idle_4.png"),
-                         "run": pygame.image.load("data/Pink_Monster_Run_6.png")}
+    player_animations = {"idle": load_image("Pink_Monster_Idle_4.png", -1),
+                         "run": load_image("Pink_Monster_Run_6.png", -1),
+                         "attack": load_image("Pink_Monster_Attack1_4.png", -1)}
 
-    player = Player(50, 50, 0, 500, player_animations)
     floor = Wall(width, 30, 0, height - 30)
-    block = Wall(100, 30, 300, 450)
-    s_floor = Wall(300, 50, 0, 300)
-    block_2 = Wall(300, 50, 300, 800)
-    block_3 = Wall(300, 50, 400, 700)
-    block_4 = Wall(300, 50, 500, 600)
-
-    objects.append(block)
-    objects.append(floor)
-    objects.append(s_floor)
-    objects.append(block_2)
-    objects.append(block_3)
-    objects.append(block_4)
+    block = Wall(300, 30, 200, 800)
+    player = Player(64, 64, 0, 300, player_animations)
 
     interface = Interface(player)
 
@@ -307,20 +281,14 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    player.jump()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 player.shoot()
 
-        screen.fill((0, 0, 0))
+        screen.fill((100, 0, 0))
 
-        for el in objects:
-            el.render(screen)
+        all_sprites.update()
 
-        interface.render(screen)
-
-        player.render(screen, keys)
+        interface.render()
 
         pygame.display.flip()
         clock.tick(fps)
