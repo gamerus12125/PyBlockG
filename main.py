c44e95380183cc8c7316935dd49fd2c546fcbd20
gamerus12125 <Mik78456@yandex.ru>
2023-12-27 21:02:30 +0300

    added particles, fireball attack, changed player sprite

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pygame
 import os
 import time
+import random
 
-all_sprites = pygame.sprite.Group()
 WIDTH = 1000
 HEIGHT = 1000
+GRAVITY = 0.2
+
+all_sprites = pygame.sprite.Group()
+screen_rect = (0, 0, WIDTH, HEIGHT)
 
 
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
     try:
-        image = pygame.image.load(fullname).convert()
+        image = pygame.image.load(fullname)
     except pygame.error as message:
-        print('Cannot load image:', name)
         raise SystemExit(message)
 
     if color_key is not None:
@@ -22,6 +25,29 @@ def load_image(name, color_key=None):
     else:
         image = image.convert_alpha()
     return image
+
+
+class Particle(pygame.sprite.Sprite):
+    def __init__(self, pos, dx, dy):
+        super().__init__(all_sprites)
+        self.fire = [load_image("star.png")]
+        for scale in (5, 10, 20):
+            self.fire.append(pygame.transform.scale(self.fire[0], (scale, scale)))
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+
+        self.velocity = [dx, dy]
+        self.rect.x, self.rect.y = pos
+
+        self.gravity = GRAVITY
+
+    def update(self):
+        self.velocity[1] += self.gravity
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        if not self.rect.colliderect(screen_rect):
+            self.kill()
+        screen.blit(self.image, self.rect)
 
 
 class Interface:
@@ -76,19 +102,50 @@ class Wall(pygame.sprite.Sprite):
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, direction):
         super().__init__(all_sprites)
-        self.rect = pygame.Rect(x, y, 20, 10)
         self.is_player_collide = False
+        self.direction = direction
         self.vx = -4 if direction == "L" else 4
+        self.frames = []
+        self.cut_sheet(load_image("All_Fire_Bullet_Pixel_16x16_00.png"), 4, 1)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = pygame.Rect(x, y, 64, 32)
+        self.animation_timer = Timer()
+        self.animation_timer.start()
+        self.animation_time = 0.1
+
+    def cut_sheet(self, sheet, columns, rows):
+        x = sheet.get_width() // columns
+        y = sheet.get_height() // rows - 1
+        if self.direction == "L":
+            for j in range(rows):
+                for i in range(columns):
+                    self.frames.append(
+                        pygame.transform.flip(
+                            pygame.transform.scale_by(sheet.subsurface(pygame.Rect(i * x, y * j, 32, 16)), 2), True,
+                            False))
+        else:
+            for j in range(rows):
+                for i in range(columns):
+                    self.frames.append(
+                        pygame.transform.scale_by(sheet.subsurface(pygame.Rect(i * x, y * j, 32, 16)), 2))
 
     def update(self):
         colliders = pygame.sprite.spritecollide(self, all_sprites, False)
         if colliders and Wall.__name__ in colliders.__str__():
-            del self
-        else:
-            pygame.draw.rect(screen, pygame.color.Color("red"), self.rect)
+            create_particles((self.rect.x, self.rect.y))
+            self.kill()
+        elif self.animation_timer.get_time() >= self.animation_time:
+            self.cur_frame = 0 if self.cur_frame + 1 == len(self.frames) else self.cur_frame + 1
+            self.image = self.frames[self.cur_frame]
+            self.animation_timer.stop()
+            self.animation_timer.start()
+        if self:
+            screen.blit(self.image, self.rect)
+            pygame.draw.rect(screen, (255, 0, 0), self.rect, 1)
             self.rect = self.rect.move(self.vx, 0)
             if self.rect.x > WIDTH:
-                del self
+                self.kill()
 
 
 class Player(pygame.sprite.Sprite):
@@ -102,7 +159,6 @@ class Player(pygame.sprite.Sprite):
         self.is_walk_l = False
         self.is_idle = True
         self.is_attack = False
-        self.g = 0.2
         self.fall_speed = 0
 
         self.ammo = 10
@@ -146,7 +202,7 @@ class Player(pygame.sprite.Sprite):
                     False))
 
     def update(self):
-        self.fall_speed += self.g
+        self.fall_speed += GRAVITY
         self.rect = self.rect.move(0, self.fall_speed)
         colliders = pygame.sprite.spritecollide(self, all_sprites, False)
         colliders.remove(self)
@@ -156,13 +212,28 @@ class Player(pygame.sprite.Sprite):
             self.is_down = False
         else:
             self.is_down = True
-
         if self.is_reload:
             if self.shoot_clock.get_time() > 1:
                 self.is_reload = False
                 self.ammo = 10
-
         self.move()
+        self.draw()
+
+    def draw(self):
+        if self.is_attack:
+            if self.direction == "L":
+                self.animation("attack_l")
+            else:
+                self.animation("attack_r")
+        elif self.is_walk_r:
+            self.animation("run_r")
+        elif self.is_walk_l:
+            self.animation("run_l")
+        else:
+            if self.direction == "L":
+                self.animation("idle_l")
+            else:
+                self.animation("idle_r")
 
     def animation(self, name):
         anim = name
@@ -170,14 +241,19 @@ class Player(pygame.sprite.Sprite):
             self.anim_cur_time.stop()
             self.anim_cur_time.start()
             self.index += 1
-
             if self.index >= len(self.images[anim]):
                 if self.is_attack:
+                    if self.direction == "L":
+                        Bullet(self.rect.x - 1, int(self.rect.y + 5), self.direction)
+                    else:
+                        Bullet(self.rect.x + self.rect.w + 1, int(self.rect.y + 5), self.direction)
                     self.is_attack = False
                 self.index = 0
-            self.image = self.images[anim][self.index]
-        if self.is_attack:
+        if self.is_attack and self.direction == "R":
             anim = "attack_r"
+        elif self.is_attack and self.direction == "L":
+            anim = "attack_l"
+        self.image = self.images[anim][self.index]
         screen.blit(self.images[anim][self.index], (self.rect.x - 16, self.rect.y - 32))
         pygame.draw.rect(screen, (255, 0, 0), self.rect, 1)
 
@@ -186,9 +262,9 @@ class Player(pygame.sprite.Sprite):
             self.fall_speed = -10
 
     def move(self):
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and not self.is_attack:
             self.jump()
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] and not self.is_attack:
             self.rect = self.rect.move(2, 0)
             self.direction = "R"
             is_collide = pygame.sprite.spritecollideany(self, all_sprites)
@@ -196,7 +272,6 @@ class Player(pygame.sprite.Sprite):
                 self.rect = self.rect.move(-2, 0)
                 if self.is_walk_l or self.is_walk_r:
                     self.index = 0
-                self.animation("idle_r")
                 self.is_walk_r = False
                 self.is_walk_l = False
                 return False
@@ -205,10 +280,9 @@ class Player(pygame.sprite.Sprite):
                 self.is_walk_r = True
                 if self.is_idle:
                     self.index = 0
-                self.animation("run_r")
                 self.is_idle = False
                 return True
-        elif keys[pygame.K_a]:
+        elif keys[pygame.K_a] and not self.is_attack:
             self.rect = self.rect.move(-2, 0)
             self.direction = "L"
             is_collide = pygame.sprite.spritecollideany(self, all_sprites)
@@ -216,7 +290,6 @@ class Player(pygame.sprite.Sprite):
                 self.rect = self.rect.move(2, 0)
                 if self.is_walk_l or self.is_walk_r:
                     self.index = 0
-                self.animation("idle_l")
                 self.is_walk_r = False
                 self.is_walk_l = False
                 return False
@@ -225,16 +298,11 @@ class Player(pygame.sprite.Sprite):
                 self.is_walk_r = False
                 if self.is_idle:
                     self.index = 0
-                self.animation("run_l")
                 self.is_idle = False
                 return True
         else:
             if self.is_walk_l or self.is_walk_r:
                 self.index = 0
-            if self.direction == "R":
-                self.animation("idle_r")
-            else:
-                self.animation("idle_l")
             self.is_walk_r = False
             self.is_walk_l = False
 
@@ -247,13 +315,16 @@ class Player(pygame.sprite.Sprite):
                 self.shoot_clock.stop()
                 self.shoot_clock.start()
             else:
-                if self.direction == "L":
-                    Bullet(self.rect.x - 1, int(self.rect.y + 5), self.direction)
-                else:
-                    Bullet(self.rect.x + self.rect.w + 1, int(self.rect.y + 5), self.direction)
                 self.ammo -= 1
                 self.shoot_clock.stop()
                 self.shoot_clock.start()
+
+
+def create_particles(position):
+    particle_count = 20
+    numbers = range(-5, 6)
+    for _ in range(particle_count):
+        Particle(position, random.choice(numbers), random.choice(numbers))
 
 
 if __name__ == '__main__':
@@ -270,6 +341,7 @@ if __name__ == '__main__':
 
     floor = Wall(width, 30, 0, height - 30)
     block = Wall(300, 30, 200, 800)
+    struc = Wall(50, 300, 700, 500)
     player = Player(64, 64, 0, 300, player_animations)
 
     interface = Interface(player)
@@ -285,7 +357,6 @@ if __name__ == '__main__':
                 player.shoot()
 
         screen.fill((100, 0, 0))
-
         all_sprites.update()
 
         interface.render()
